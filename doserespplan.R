7d9041bdb6842406f05613beca07c739859ed8f3
eastandrew <38232641+eastandrew@users.noreply.github.com>
2019-03-05 14:59:08 -0500

    add labels to pick easier

--- doserespplan.R
+++ doserespplan.R
@@ -17,3 +17,22 @@ points((c(1.004277,0.3694528,0.1359141,0.05)),c(seq(0.1,0.3,length.out=4)), pch=
 text((c(1.004277,0.3694528,0.1359141,0.05)),c(seq(0.1,0.3,length.out=4)),c("1.00","0.37","0.14","0.05"))
 points((c(1.35,0.45,0.15,0.05)),c(seq(0.0,0.2,length.out=4)), pch=NA,type="b")
 text((c(1.35,0.45,0.15,0.05)),c(seq(0.0,0.2,length.out=4)),c("1.35","0.45","0.15","0.05"))
+
+
+
+numtreats <- 4
+effectmag <- 0.2
+propdiffe <- exp(1)
+propdiff3 <- 3
+propdiff10 <- 10
+propdiff2 <- 2
+startinglow <- 0.05
+
+plot(1,1,pch=NA, ylim=c(0,1),xlim=c(startinglow,startinglow*propdiff2^(numtreats-1)),xlab="log visualized conc",ylab="effect",log="x")
+points(c(startinglow,startinglow*propdiff2,startinglow*propdiff2^2,startinglow*propdiff2^3),
+       c(seq(1,1-effectmag,length.out=numtreats)),
+       pch=NA,
+       type="b")
+text(c(startinglow,startinglow*propdiff2,startinglow*propdiff2^2,startinglow*propdiff2^3),
+     c(seq(1,1-effectmag,length.out=numtreats)),
+     c(bquote(.(startinglow)),bquote(.(startinglow*propdiff2)),bquote(.(startinglow*propdiff2^2)),bquote(.(startinglow*propdiff2^3))))
